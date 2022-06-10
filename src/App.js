cee53d034edb1f39876f06eb68349a5bd970a0b4
Evan Gentry Moore <evan.gentry.moore@gmail.com>
2022-06-10 01:58:17 -0500

    Finshed? Nah. But good enough

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, createContext} from 'react'
 import {Link, Outlet} from 'react-router-dom'
+import HomePage from './components/HomePage';
 
 import './App.css';
 
@@ -7,21 +8,22 @@ const CartContext = createContext()
 const ToggleContext = createContext()
 
 function App() {
-  const [renderHome, setRenderHome] = useState(false)
   const [cart, setCart] = useState([])
   const [toggle, setToggle] = useState(false)
+
+  const [renderHome, setRenderHome] = useState(false)
 
   return (
     <CartContext.Provider value={{ cart, setCart }}>
     <ToggleContext.Provider value={{ toggle, setToggle }}>
       <header className="nav-bar">
-        <Link to='/' className='nav-links' onClick={() => setRenderHome(true)}><h1>Site Logo</h1></Link>
+        <Link to='/' className='nav-links' onClick={() => setRenderHome(true)}><h2>Site Logo</h2></Link>
         <nav className='nav-section'>
           <Link to='/menu' className='nav-links' onClick={() => setRenderHome(false)}>Menu</Link>|
           <Link to='/cart' className='nav-links' onClick={() => setRenderHome(false)}>Cart</Link>
         </nav>
       </header>
-      {renderHome ? <h1>Place Holder for Home Component</h1> : <Outlet />}
+      {renderHome ? <HomePage /> : <Outlet />}
     </ToggleContext.Provider>
     </CartContext.Provider>
   )
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.css"
+
+function HomePage() {
+  return (
+    <>
+      <h1>Evan Moore</h1>
+      <h1>Devmountain</h1>
+      <h1>WebDev</h1>
+      <h1>Capstone</h1>
+      <h3>(Function over form this time)</h3>
+    </>
+  )
+}
+
+export default HomePage
